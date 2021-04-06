7484204d09a84d8cf5a0126080adfa7ba46aa672
Nguyen Tuan Minh <nguyentuanminh.cn@gmail.com>
2021-04-06 20:24:42 +0700

    last one

---
+++ README.md
@@ -0,0 +1,17 @@
+// Nodemon
+
+- install node-mon
+
+* npm install -g nodemon: "install trong mọi dự án"
+* npm install --save-dev nodemon: "install trong devDependencies của dự án"
+
+- Vào file package.json chọn scripts viết ("start": "nodemon --inspect ./build/index.js",): "chạy npm start để không thoát khỏi command") npm start
+  //Morgan
+
+- install morgan
+
+* npm install --save-dev nodemon: "install trong devDependencies của dự án"
+  // node-sass
+
+- install scss: npm install node-sass
+- Vào file package.json chọn scripts viết ("watch": "node-sass -w build/resource/scss/app.scss build/public/css/app.css",) //node-sass -w file chứa scss sang file chứa css npm run watch
--- build/index.js
+++ build/index.js
@@ -1,8 +1,23 @@
+var path = require("path");
 var express = require("express");
+var morgan = require("morgan");
+var handlebars = require("express-handlebars");
 var app = express();
 var port = 3000;
+app.use(express.static(path.join(__dirname, "public/img")));
+//HTTP logger
+app.use(morgan("combined"));
+//template engine
+app.engine("hbs", handlebars({
+    extname: ".hbs",
+}));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resource/views"));
 app.get("/", function (req, res) {
-    res.send("Hello World!");
+    res.render("home");
+});
+app.get("/news", function (req, res) {
+    res.render("news");
 });
 app.listen(port, function () {
     console.log("Example app listening at http://localhost:" + port);
